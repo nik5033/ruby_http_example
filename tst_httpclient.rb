8b7919c18b65b37c37f455dd4759c473f1a8fb97
Nikita Gudkov <a@a.ru>
2021-10-11 19:37:12 +0200

    Done p-12

--- tst_httpclient.rb
+++ tst_httpclient.rb
@@ -2,7 +2,10 @@ require 'httpclient'
 require 'json'
 
 client = HTTPClient.new
-response = client.request(:put, 'https://postman-echo.com/put')
+body = 'This is expected to be sent back as part of response body.'
+uri = 'https://postman-echo.com/put'
+response = client.put(uri, body)
+
 result = JSON.parse(response.body)
 
 puts result
