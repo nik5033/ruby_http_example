07073afa9627b1edee894748ccf0d13e95c9b5cc
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-10-12 19:34:04 +0300

    p-13 3 ex add /sum path

--- web_server.rb
+++ web_server.rb
@@ -5,26 +5,25 @@ server = TCPServer.new 5678
 
 while session = server.accept
    request = session.gets
-   puts request
 
    first_http = request.split # 1 - method, 2 - path, 3-HTTP
-   reg = /[?].*/.match(first_http[1])
-   puts CGI::parse(first_http[1])
-
+   
    session.print "HTTP/1.1 200\r\n"
    session.print "Content-Type: text/html\r\n"
    session.print "\r\n"
 
    case first_http[1]
    when '/time'
-    session.print "Hello world! The time is #{Time.now}"
+      session.print "Hello world! The time is #{Time.now}"
    when '/hello'
-    session.print "hello"
+      session.print "hello"
+   when /\/sum\?*/
+      /[?](?<param>.*)/ =~ first_http[1]
+      params = CGI::parse(param)
+      session.print params["a"][0].to_i + params["b"][0].to_i
    else
     session.print "Error"
    end
 
    session.close
 end
-
-#[?].*
